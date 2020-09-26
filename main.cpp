ccae0975f3d1b663465673a0b1fbe0e101fcca28
jameskkk <chenjhkk@gmail.com>
2020-09-26 14:09:28 +0800

    Import CPlusPlus into github.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    printf("Hello World!\n");
+    cout << "Hello World!" << endl;
+
+    system("PAUSE");
+    return 0;
+}
